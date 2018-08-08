49b1fdedb30076a665994b4b195419d670be3fe5
ktndwn <ktndwn@users.noreply.github.com>
2018-08-07 22:01:47 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 
 
 
-let linkWithoutTitle = '<div class="link" id="showcase-link">SHOWCASE</div><h1 id="placeholder-title">Displace Digital</h1><div class="link" id="contact-link">CONTACT</div>'
-let linkWithTitle = '<div class="link" id="showcase-link">SHOWCASE</div><h1 class="link" id="title-link">Displace Digital</h1><div class="link" id="contact-link">CONTACT</div>'
+let linkWithoutTitle = '<div class="link" id="showcase-link">OUR WORK</div><h1 id="placeholder-title">Displace Digital</h1><div class="link" id="contact-link">CONTACT</div>'
+let linkWithTitle = '<div class="link" id="showcase-link">OUR WORK</div><h1 class="link" id="title-link">Displace Digital</h1><div class="link" id="contact-link">CONTACT</div>'
 let above = true
+
 let linkListener = () => {
     document.getElementById('showcase-link').addEventListener('click', ()=> {
         document.getElementById('showcase-container').scrollIntoView({
@@ -33,7 +34,7 @@ linkListener()
 
 
 window.addEventListener('scroll', function(e) {
-    videoContainerHalfHeight = document.getElementById('video-container').clientHeight/2.1
+    videoContainerHalfHeight = document.getElementById('video-container').clientHeight/3.1
     windowHeight = window.scrollY
     if (videoContainerHalfHeight > windowHeight && above == false) {
         above = true
